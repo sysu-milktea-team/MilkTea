f9a9694cb54526623e95c6820c37714f98f57629
Xuex1997 <1142166269@qq.com>
2018-06-26 12:16:39 +0800

    add start UI

---
+++ HelloAR/Assets/MilkTea/Scripts/home.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class home : MonoBehaviour {
+
+	public GameObject Text;
+
+
+	// Use this for initialization
+	void Awake () {
+		Button button = gameObject.GetComponent<Button>() as Button;//获取Button组件
+		button.onClick.AddListener(myClick);//为button的OnClick事件添加监听器，当监听到Click事件时，回调myClick函数。
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+	void myClick(){
+		Text.GetComponent<Text> ().text = "this is the introduction of the game ";
+	}
+}
+
